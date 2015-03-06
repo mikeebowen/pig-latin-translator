2a5c16705edd0664cc66abe6c467dda38f5aa1c5
Michael Bowen <mikee.bowen@gmail.com>
2015-03-06 13:33:42 -0800

    add working pig latinify function

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  $("#word-to-translate") = wordToTranslate;
+  $("#translate-button").on("click", function(){
+    // create variable for pig latin word
+    var pigLatinWord = "";
+    // create variable to hold first letter
+    var firstLetter;
+    // split entered word into array of leters and assign input from #word-to-translate to variable
+    var $wordToTranslate = $("#word-to-translate").val().split("");
+    // assign first letter in array to variable and remove it from array
+    firstLetter = $wordToTranslate.shift();
+    // add first letter to end or array
+    $wordToTranslate.push(firstLetter);
+    // turn array back into string
+    pigLatinWord = $wordToTranslate.join("");
+    $("#pig-latin-words").append("<li>" + pigLatinWord + "ay" + "</li>");
+  })
 });
