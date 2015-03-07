06f6c3a075fb9a30c1c9acd6488dfa21d673d666
Michael Bowen <mikee.bowen@gmail.com>
2015-03-07 13:56:00 -0800

    Make vowels have different pig latin

--- script.js
+++ script.js
@@ -7,11 +7,20 @@ $(document).ready(function() {
     // split entered word into array of leters and assign input from #word-to-translate to variable
     var $wordToTranslate = $("#word-to-translate").val().split("");
     // assign first letter in array to variable and remove it from array
-    firstLetter = $wordToTranslate.shift();
-    // add first letter to end or array
-    $wordToTranslate.push(firstLetter);
-    // turn array back into string
-    pigLatinWord = $wordToTranslate.join("");
-    $("#pig-latin-words").append("<li>" + pigLatinWord + "ay" + "</li>");
+    firstLetter = $wordToTranslate.shift().toLowerCase();
+    // make if statement for vowels
+    if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
+      // add letter back to beginning of array
+      $wordToTranslate.unshift(firstLetter);
+      // turn array back into string
+      pigLatinWord = $wordToTranslate.join("");
+      $("#pig-latin-words").append("<li>" + pigLatinWord + "way" + "</li>");
+    } else {
+      // add first letter to end or array
+      $wordToTranslate.push(firstLetter);
+      // turn array back into string
+      pigLatinWord = $wordToTranslate.join("");
+      $("#pig-latin-words").append("<li>" + pigLatinWord + "ay" + "</li>");
+    }
   })
 });
